c9383b7e5ba6cf3ca199391e8435d81381efd5fc
Joe Teates <creepygoat342@gmail.com>
2024-01-23 22:46:34 -0500

    added some a slideshow of images, still very rough draft and nothing is even close to done

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let slideIndex = 0;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+function showSlides(n) {
+    slideIndex = n;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    console.log(slides.length);
+  if (n >= slides.length) {slideIndex = 0}
+  if (n < 0) {slideIndex = slides.length-1}
+  for (var i = 0; i < slides.length; i++) {
+      slides.item(i).classList.add("hidden");
+    }
+    slides.item(slideIndex).classList.remove("hidden");
+    for (var i = 0; i < dots.length; i++) {
+        dots.item(i).classList.remove("selected");
+    }
+    dots.item(slideIndex).classList.add("selected");
+
+}
